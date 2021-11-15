dffc3733793e02f8af026bb3d950ee75a4d34005
resting3 <88263665+resting3@users.noreply.github.com>
2021-11-15 20:35:45 +0100

    Add files via upload

---
+++ rickroll.py
@@ -0,0 +1,3 @@
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=iik25wqIuFo&ab_channel=Rickroll%2Cbutwithadifferentlink")
+webbrowser.get("chrome").open("https://www.youtube.com/watch?v=iik25wqIuFo&ab_channel=Rickroll%2Cbutwithadifferentlink")
